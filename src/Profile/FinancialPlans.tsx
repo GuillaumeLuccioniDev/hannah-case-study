9944af794d761423f5893bdb3a077e25d6e1296d
Guillaume Luccioni <guillaume.luccioni@gmail.com>
2023-04-27 00:34:11 +0200

    add unmount modal on hide

--- src/Profile/FinancialPlans.tsx
+++ src/Profile/FinancialPlans.tsx
@@ -11,13 +11,14 @@ export function FinancialPlans() {
   return (
     <div>
       <h2 className="pb-2 border-bottom">Financial Plans</h2>
-      <AddFinancialPlanModal
-        show={showModal}
-        onClose={toggleShowModal}
-        onConfirm={() => {
-          toggleShowModal();
-        }}
-      />
+      {showModal && (
+        <AddFinancialPlanModal
+          onClose={toggleShowModal}
+          onConfirm={() => {
+            toggleShowModal();
+          }}
+        />
+      )}
       <Row xs={1} sm={2} md={2} lg={3} xl={4} xxl={5} className="g-4">
         <Col>
           <FinancialPlanAddCard onClick={toggleShowModal} />
