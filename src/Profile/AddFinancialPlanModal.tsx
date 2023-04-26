b60803c29036d9a8bcbebd42e33907b8ccfb0ace
Guillaume Luccioni <guillaume.luccioni@gmail.com>
2023-04-27 00:33:48 +0200

    add form validation

--- src/Profile/AddFinancialPlanModal.tsx
+++ src/Profile/AddFinancialPlanModal.tsx
@@ -1,36 +1,58 @@
+import { useRef } from "react";
 import { Button, Form, Modal, Stack } from "react-bootstrap";
+import { SubmitHandler, useForm } from "react-hook-form";
 
 export interface AddFinancialPlanModalProps {
-  show: boolean;
-  onConfirm: () => void;
+  onConfirm: (data: IFormInput) => void;
   onClose: () => void;
 }
 
-export function AddFinancialPlanModal({
-  show,
-  onConfirm,
-  onClose,
-}: AddFinancialPlanModalProps) {
+interface IFormInput {
+  email: string;
+  legalFormId: string;
+}
+
+export function AddFinancialPlanModal({ onConfirm, onClose }: AddFinancialPlanModalProps) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, touchedFields, isSubmitted },
+  } = useForm<IFormInput>({ defaultValues: { email: "", legalFormId: "" } });
+
+  const formRef = useRef<HTMLFormElement>(null);
+
   return (
-    <Modal show={show} onHide={onClose}>
+    <Modal show onHide={onClose}>
       <Modal.Header closeButton>
         <Modal.Title>Add Financial Plan</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <Form>
+        <Form ref={formRef} onSubmit={handleSubmit(onConfirm)}>
           <Stack gap={3}>
             <Form.Group>
               <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" autoFocus />
+              <Form.Control
+                type="email"
+                autoFocus
+                {...register("email", { required: "This field is required" })}
+                isInvalid={(touchedFields.email || isSubmitted) && !!errors.email}
+              />
+              {errors.email && <Form.Control.Feedback type="invalid">{errors.email.message}</Form.Control.Feedback>}
             </Form.Group>
             <Form.Group>
-              <Form.Label>Example textarea</Form.Label>
-              <Form.Select>
-                <option>Open this select menu</option>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
+              <Form.Label>Legal Form</Form.Label>
+              <Form.Select
+                {...register("legalFormId", { required: "This field is required" })}
+                isInvalid={(touchedFields.legalFormId || isSubmitted) && !!errors.legalFormId}
+              >
+                <option value="">Open this select menu</option>
+                <option value={1}>One</option>
+                <option value={2}>Two</option>
+                <option value={3}>Three</option>
               </Form.Select>
+              {errors.legalFormId && (
+                <Form.Control.Feedback type="invalid">{errors.legalFormId.message}</Form.Control.Feedback>
+              )}
             </Form.Group>
           </Stack>
         </Form>
@@ -39,7 +61,7 @@ export function AddFinancialPlanModal({
         <Button variant="secondary" onClick={onClose}>
           Cancel
         </Button>
-        <Button variant="primary" onClick={onConfirm}>
+        <Button variant="primary" onClick={() => formRef.current?.requestSubmit()}>
           Confirm
         </Button>
       </Modal.Footer>
